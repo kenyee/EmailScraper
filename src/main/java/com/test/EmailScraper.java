e1520a6f8a8eed4974c5a685a37b21c6188d4954
Ken Yee <kenkyee@excite.com>
2016-03-02 00:43:48 -0500

    add command line parameter for -noajax

--- src/main/java/com/test/EmailScraper.java
+++ src/main/java/com/test/EmailScraper.java
@@ -7,8 +7,17 @@ import com.test.utils.SslUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.kohsuke.args4j.ExampleMode.ALL;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.BooleanOptionHandler;
+
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -18,9 +27,24 @@ public class EmailScraper {
 	
 	private static final Logger logger = LoggerFactory.getLogger(EmailScraper.class);
 
+	@Argument
+	private List<String> arguments = new ArrayList<String>();
+
+	@Option(name="-noajax",usage="Disables support for AJAX web sites")
+	private boolean noAjax;
+
 	public static void main(String[] args) {
-		if ((args.length == 0) || (args.length > 1)) {
-			System.out.println("Usage: EmailScraper <hostname>");
+		new EmailScraper().runMain(args);
+	}
+
+	private void runMain(String[] args) {
+		CmdLineParser parser = new CmdLineParser(this);
+		try {
+			parser.parseArgument(args);
+			if( arguments.isEmpty() )
+				throw new CmdLineException("Must specify site");
+		} catch( CmdLineException e ) {
+			System.err.println("  Example: java -jar EmailParser-1.0.jar <website>"+parser.printExample(ALL));
 			System.exit(0);
 		}
 
@@ -33,12 +57,12 @@ public class EmailScraper {
 			logger.warn("Unable to trust all SSL connections");
 		}
 
-		Scraper scraper = new JsoupScraper(true);
+		Scraper scraper = new JsoupScraper(!noAjax);
 		Set<String> emails = null;
 		try {
-			emails = scraper.getEmails(args[0]);
+			emails = scraper.getEmails(arguments.get(0));
 		} catch (Exception e) {
-			logger.error("Error scraping pages" + e);
+			logger.error("Error scraping pages: ", e);
 			System.exit(-2);
 		}
 
