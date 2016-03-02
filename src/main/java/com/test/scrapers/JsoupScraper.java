c3f83dd7ad46ab0df3354bec3149b62f3216a920
Ken Yee <kenkyee@excite.com>
2016-03-02 00:44:36 -0500

    changes from PR review

--- src/main/java/com/test/scrapers/JsoupScraper.java
+++ src/main/java/com/test/scrapers/JsoupScraper.java
@@ -7,7 +7,6 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.openqa.selenium.WebDriver;
 import com.machinepublishers.jbrowserdriver.Timezone;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings;
@@ -31,7 +30,11 @@ public class JsoupScraper implements Scraper {
     private final Set<String> visitedUrls = new HashSet<>();
     private final Set<String> emails = new HashSet<>();
     private final Pattern emailRegEx = Pattern.compile("[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+");
+
+    private String baseDomain;
+
     private final boolean useWebClient;
+    private JBrowserDriver browserDriver;
 
     public JsoupScraper(boolean useWebClient) {
         this.useWebClient = useWebClient;
@@ -49,7 +52,19 @@ public class JsoupScraper implements Scraper {
             System.exit(-1);
         }
 
+        // calculate root domain...we only want the last two sections of the hostname
+        String[] levels = rootUri.getHost().split("\\.");
+        if (levels.length > 1)
+        {
+            baseDomain = levels[levels.length - 2] + "." + levels[levels.length - 1];
+        }
+
         try {
+            if (useWebClient) {
+                browserDriver = new JBrowserDriver(Settings.builder().
+                        timezone(Timezone.AMERICA_NEWYORK).build());
+            }
+
             scrapeEmailsFromPage(rootUri, url);
         } catch (Exception ex) {
             if (emails.size() == 0) {
@@ -58,54 +73,33 @@ public class JsoupScraper implements Scraper {
                 // just log a debug warning because we got some useful results back
                 logger.warn("Could not finish scraping site: " + ex);
             }
+        } finally {
+            if (useWebClient) {
+                // Close the browser. Allows the Selenium thread to terminate.
+                browserDriver.quit();
+            }
         }
 
         return emails;
     }
 
     private void scrapeEmailsFromPage(URI rootUri, String path) throws Exception {
-        if (path.startsWith("javascript:") || path.startsWith("#")) {
-            return;
-        }
-        if (path.startsWith("mailto:")) {
-            String email = path.substring("mailto".length()+1);
-            emails.add(email);
+        if (!shouldFollowPath(rootUri, path)) {
             return;
         }
 
-        String url;
-        if (path.contains("://")) {
-            URI pathUri = new URI(path);
-            if (!pathUri.getHost().contains(rootUri.getHost())) {
-                // don't follow links off site
-                return;
-            }
-            url = path;
-        } else {
-            url = rootUri.toString();
-            if (!path.startsWith("/")) {
-                url += "/";
-            }
-            url += path;
+        String url = getUrlToFollow(rootUri, path);
+        if (url == null) {
+            return;
         }
 
-        // strips off protocol header
-        URI uri = new URI(url);
-        String localPath = uri.getSchemeSpecificPart() + uri.getRawPath();
-        if ((localPath.length() > 0) && visitedUrls.contains(localPath)) {
-            // prevent circular loops
-            return;
-        } else {
-            visitedUrls.add(localPath);
-        }
+        System.out.println("Scraping " + url);
         logger.debug("Scraping " + url);
         System.out.print(".");  // progress indicator
 
         Document doc = useWebClient ?
                 Jsoup.parse(loadPageSelenium(url))
                 : Jsoup.connect(url).get();
-
-        logger.debug("Document is " + doc.outerHtml());
 
         // extract emails
         Matcher matcher = emailRegEx.matcher(doc.text());
@@ -132,6 +126,64 @@ public class JsoupScraper implements Scraper {
         }
     }
 
+    private String getUrlToFollow(URI rootUri, String path) throws URISyntaxException {
+        String url;
+        if (path.contains("://")) {
+            url = path;
+        } else {
+            url = rootUri.toString();
+            if (!path.startsWith("/")) {
+                url += "/";
+            }
+            url += path;
+        }
+
+        // strips off protocol header
+        try {
+            URI uri = new URI(url.trim());
+            String localPath = uri.getSchemeSpecificPart() + uri.getRawPath();
+            if ((localPath.length() > 0) && visitedUrls.contains(localPath)) {
+                // prevent circular loops
+                return null;
+            } else {
+                visitedUrls.add(localPath);
+            }
+        } catch (URISyntaxException e) {
+            System.err.println("Ignoring bad URL: " + path);
+            return null;
+        }
+
+        return url;
+    }
+
+    private boolean shouldFollowPath(URI rootUri, String path) throws URISyntaxException {
+        if (path.startsWith("javascript:") || path.startsWith("#")) {
+            return false;
+        }
+        if (path.startsWith("mailto:")) {
+            String email = path.substring("mailto".length()+1);
+            emails.add(email);
+            return false;
+        }
+
+        // don't follow links off site
+        try {
+            // get base domain of root URI
+            // NOTE: this only works w/ real root domains.  If you have a site on a shared
+            // hosting service like mysite.squarespace.com, this check will let us walk any
+            // domains on the site :-(
+            URI pathUri = new URI(path.trim());
+            if ((pathUri.getHost() != null)
+                    && !pathUri.getHost().endsWith(baseDomain)) {
+                return false;
+            }
+        } catch (URISyntaxException e) {
+            System.err.println("Ignoring bad URL: " + path);
+            return false;
+        }
+        return true;
+    }
+
     private String loadPageSelenium(String url) {
         // this can throw a lot of different exceptions
 
@@ -139,24 +191,18 @@ public class JsoupScraper implements Scraper {
         Properties props = System.getProperties();
         props.setProperty("jbd.pemfile", "trustanything");
 
-        JBrowserDriver driver = new JBrowserDriver(Settings.builder().
-                timezone(Timezone.AMERICA_NEWYORK).build());
-
         // This will block for the page load and any
         // associated AJAX requests
-        driver.get(url);
+        browserDriver.get(url);
 
         // ignore pages w/ errors
-        if (driver.getStatusCode() != 200) {
+        if (browserDriver.getStatusCode() != 200) {
             return "";
         }
 
         // Returns the page source in its current state, including
         // any DOM updates that occurred after page load
-        String result = driver.getPageSource();
-
-        // Close the browser. Allows the Selenium thread to terminate.
-        driver.quit();
+        String result = browserDriver.getPageSource();
 
         return result;
     }
